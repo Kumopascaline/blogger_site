0c90ce6e092e7513b8a54d34415525c2a496f5f9
Kumopascaline <75366612+Kumopascaline@users.noreply.github.com>
2023-11-06 17:35:48 +0100

    Third Commit (routing to blog details)

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -1,10 +1,30 @@
 import { useParams } from "react-router-dom/cjs/react-router-dom.min";
+import useFetch from "./useFetch";
+import wait from "./wait.gif";
 
 const BlogDetails = () => {
   const { id } = useParams();
+  const {
+    data: blog,
+    isPending,
+    error,
+  } = useFetch("http://localhost:8000/blogs/" + id);
   return (
     <div className="blog-details">
-      <h2>Blog Details - {id}</h2>
+      {error && <div>{error}</div>}
+      {isPending && (
+        <div>
+          <img className="pending" src={wait} alt="loading spinner"></img>
+        </div>
+      )}
+
+      {blog && (
+        <article>
+          <h2>{blog.title}</h2>
+          <div>{blog.body}</div>
+          <h2>Written By: {blog.author}</h2>
+        </article>
+      )}
     </div>
   );
 };
